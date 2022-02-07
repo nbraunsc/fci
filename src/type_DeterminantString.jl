a4cae0906cee212936a622621ac08d454f67898f
Nick Mayhall <nmayhall@vt.edu>
2022-02-07 15:36:14 -0500

    test

--- src/type_DeterminantString.jl
+++ src/type_DeterminantString.jl
@@ -1,9 +1,21 @@
 using StaticArrays
 
 struct DeterminantString{N}
-    norbs::Int
-    nelec::Int
-    config::SVector{N, Int}
-    #config::MVector{N, UInt}
-    index::Int
+    norbs::UInt8
+    nelec::UInt8
+    config::SVector{N, UInt8}
+    #config::MVector{N, UInt8}
+    index::UInt
 end
+
+function DeterminantString(norbs, nelec, in::AbstractArray{T,1}, index) where T 
+    return DeterminantString{length(in)}(convert(UInt8, norbs), 
+                                  convert(UInt8, nelec), 
+                                  ntuple(i -> convert(UInt8, in[i]), length(in)),
+                                  convert(UInt, index)
+                                 )
+end
+
+"""
+    function Base.convert(
+"""
